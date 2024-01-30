ca57f469f4b3648a29dc106f4c05aec909d49563
lkm00 <lkm00@outlook.com>
2024-01-30 05:27:35 -0600

    Core

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,8 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Explorer));
+            this.TopBarDrag = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.TopBar = new System.Windows.Forms.Panel();
+            this.Exit = new System.Windows.Forms.Button();
+            this.ExplorerTree = new System.Windows.Forms.TreeView();
+            this.TopBar.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // TopBarDrag
+            // 
+            this.TopBarDrag.Fixed = true;
+            this.TopBarDrag.Horizontal = true;
+            this.TopBarDrag.TargetControl = this.TopBar;
+            this.TopBarDrag.Vertical = true;
+            // 
+            // TopBar
+            // 
+            this.TopBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.TopBar.Controls.Add(this.Exit);
+            this.TopBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopBar.Location = new System.Drawing.Point(0, 0);
+            this.TopBar.Name = "TopBar";
+            this.TopBar.Size = new System.Drawing.Size(958, 33);
+            this.TopBar.TabIndex = 0;
+            // 
+            // Exit
+            // 
+            this.Exit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Exit.FlatAppearance.BorderSize = 0;
+            this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit.ForeColor = System.Drawing.Color.Red;
+            this.Exit.Location = new System.Drawing.Point(916, 0);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(42, 33);
+            this.Exit.TabIndex = 0;
+            this.Exit.Text = "X";
+            this.Exit.UseVisualStyleBackColor = true;
+            this.Exit.Click += new System.EventHandler(this.Exit_Click);
+            // 
+            // ExplorerTree
+            // 
+            this.ExplorerTree.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.ExplorerTree.Dock = System.Windows.Forms.DockStyle.Top;
+            this.ExplorerTree.Location = new System.Drawing.Point(0, 33);
+            this.ExplorerTree.Name = "ExplorerTree";
+            this.ExplorerTree.Size = new System.Drawing.Size(958, 202);
+            this.ExplorerTree.TabIndex = 1;
+            this.ExplorerTree.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
             // 
             // Explorer
             // 
@@ -37,15 +85,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
             this.ClientSize = new System.Drawing.Size(958, 287);
+            this.Controls.Add(this.ExplorerTree);
+            this.Controls.Add(this.TopBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Explorer";
             this.Text = "Explorer";
+            this.TopBar.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Bunifu.Framework.UI.BunifuDragControl TopBarDrag;
+        private System.Windows.Forms.Panel TopBar;
+        private System.Windows.Forms.Button Exit;
+        private System.Windows.Forms.TreeView ExplorerTree;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,85 @@ namespace RobloxExternalExplorer
 {
     public partial class Explorer : Form
     {
+        string RobloxCacheStorage = $@"C:\Users\{Environment.UserName}\AppData\Local\Temp\Roblox";
+        private ImageList ImageAssets;
+
+        private Dictionary<string, int> TreeNodes = new Dictionary<string, int>();
         public Explorer()
         {
             InitializeComponent();
+            if (Directory.Exists(RobloxCacheStorage) == false)
+            {
+                throw new Exception("Cannot Find ROBLOX CACHE, Please Check If Roblox Is Installed Or Reinstall The Program");
+            }
+            string AudioCache = $@"{RobloxCacheStorage}\sounds";
+            string HttpsCache = $@"{RobloxCacheStorage}\http";
+            ImageAssets = new ImageList();
+
+            ImageAssets.Images.Add("SOUND", Properties.Resources.Sound);
+            ImageAssets.Images.Add("HTTPS", Properties.Resources.Https);
+            ImageAssets.Images.Add("FILE", Properties.Resources.File);
+            ExplorerTree.ImageList = ImageAssets;
+            int CreateSuperClass(string Name, int ImageIndex)
+            {
+                int Info = ExplorerTree.Nodes.Add(new TreeNode(Name, imageIndex: ImageIndex, ImageIndex));
+                TreeNodes[Name] = Info;
+                return Info;
+            }
+
+            void ResetTree()
+            {
+                ExplorerTree.Nodes.Clear();
+                CreateSuperClass("Audio", 0);
+                CreateSuperClass("Https", 1);
+            }
+
+            void ClearSuperClass(string SuperClass)
+            {
+                TreeNode Node = ExplorerTree.Nodes[TreeNodes[SuperClass]];
+
+                Node.Nodes.Clear();
+            }
+
+            void AddNodeToSuperClass(string SuperClass, string Name, string FilePath)
+            {
+                TreeNode Node = ExplorerTree.Nodes[TreeNodes[SuperClass]];
+
+                int NodeIndex = Node.Nodes.Add(new TreeNode(Name, imageIndex: 2, selectedImageIndex: 2));
+
+                Node.Nodes[NodeIndex].Tag = FilePath;
+
+            }
+
+
+            ResetTree();
+            AddNodeToSuperClass("Audio", "FILE.ogg", "GAY MONSTER");
+            AddNodeToSuperClass("Https", "Log.txt", "HEHEHEHAW");
+            ClearSuperClass("Https");
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            if (e.Node.FullPath != "Audio" && e.Node.FullPath != "Https")
+            {
+                Console.WriteLine(Environment.UserName);
+                Console.WriteLine("Non SuperConstructer Node found");
+                if (e.Node.FullPath.Contains("Audio"))
+                {
+                    Console.WriteLine("Audio File Clicked on");
+                    Console.WriteLine(e.Node.Tag);
+                }
+                else
+                {
+                    Console.WriteLine("HTTP Log File Clicked on");
+                    Console.WriteLine(e.Node.Tag);
+                }
+            }
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
